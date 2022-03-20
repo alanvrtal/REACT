063bee2792a3bae8b8f4384ed6df1694cfbd53a7
Alan Vrtal <alanvrtal@hotmail.com>
2022-03-20 07:53:45 -0300

    Agregado My first app y Aprendiendo React.js

---
+++ REACT/AprendiendoReact.js
@@ -0,0 +1,34 @@
+// REACT utiliza las ultimas versiones de JS de EMAC6
+// Utiliza principalmente HTML, JS, Node.js, ES6+
+//REACT es una bibiloteca de JS que nos permite generar interfaces, elementos en pantalla, botones y navegaciones.
+//Es muy utilizada y hay mucho codigo OPEN-SOURCE
+/* REACT tiene un buen rendimiento, las apps se ejecutan de forma veloz 
+por causa del VIRTUAL DOM. */
+
+/*VENTAJAS DE REACT AL ESCRIBIR CODIGO:
+1. Podemos ver lo que escribimos.
+2. Cambiar las cosas en partes (componentes)
+3. Se puede utiliza desde el servidor. (Server Side Rendering) */
+
+/*COMPONENTES:
+1. Permite dividir un codigo en pequeñas partes, que se comportan independientes.
+2. Se reutilizan en multiples Apps.
+
+/* HERRAMIENTAS RELACIONADAS:
+-REACT Native (Es una variacion para crear aplicaciones moviles)
+-Next.js (Server Side Rendering, te da todo escrito para que solo desarrolles la app)
+-GraphQL (Lenguaje para solicitar datos y especificar que es lo que necesitas)
+-Redux (Es una manejador de estados, una bibioteca que administra el estado de una app)
+-MERN (Es un termino que define multiples tecnologias, Mongodb, Express, React, Nodejs.
+Esta relacionado a que solo se necesita escribir en JS para construir una app)
+-Ionic4 (Framework agnostico, permite crear una app movil con lo mismo que REACT)*/
+
+/* INSTALACION:
+1. Bajar NodeJs y instalarlo. (Va a instalar node y npm)
+2.abrir consola y instalarlo con este comando:
+npx create-react-app nombre-app
+
+npm start: (comienza el desarrollo del server)
+npm run build: (Nos permite construir nuestra app, transformando los codigos de react a JS,HTML Y CSS) */
+
+/* npm start (con la direccion puesta del programa y dandole start comienza a ejecutarse)
